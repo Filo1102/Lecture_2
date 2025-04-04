301a7decbf6fc29c9bd3f0aa3124a571bac10962
Filo1102 <f.toniolo11@gmail.com>
2025-04-04 10:14:20 +0000

    initial

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Metric")
+
+st.metric(label="Number of Assists", value="25", delta="+6")
+
+st.metric(label="Goals", value="13", delta="-5")
+
+#dataframe
+st.title(":blue[dataframe]")
+
+data={
+    "Player's Name": ["Pelè", "Maradona", "Baggio"],
+    "Goals": [13, 25, 24],
+    "Team" :["Juventus", "Napoli", "Inter"]
+}
+
+df=pd.DataFrame(data)
+
+st.subheader("Dataframe")
+st.dataframe(df)  #display dataframe
+
+st.subheader("Static Table")
+st.table(df) 
